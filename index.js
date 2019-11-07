f24e9b9342b7388a59d6eec5ffcf76bfc855e742
kat-star <katherinewkwong@gmail.com>
2019-11-07 13:05:20 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function (string = 'go to the office') {
+  return `This Monday, I will ${string}.`
+}
+
+function wrapAdjective(flair = '*') {
+  return function(status = 'special') {
+    return `You are ${flair}${status}${flair}!`
+  }
+}
+
+//each function should be a key in the object
+
+const Calculator = {
+  add: function(a, b) {
+    return a + b;
+  },
+  subtract: function(a, b) {
+    return a - b;
+  },
+  multiply: function(a, b) {
+    return a * b;
+  },
+  divide: function(a, b) {
+    return a / b;
+  }
+}
+
+function actionApplyer(start, array) {
+  if (array.length === 0) {
+    return start;
+  } else {
+    return ((start * 2) + 1000) % 7;
+  }
+}
